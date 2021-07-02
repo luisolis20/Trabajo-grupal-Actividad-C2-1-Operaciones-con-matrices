5bb7c0761c380c083f6d883928af2a52acd326ec
Anni Giler <85976263+Annigiler@users.noreply.github.com>
2021-07-02 18:50:58 -0500

    Update colaborador1.h
    Yo soy el colaborador 1 ANNI GILER, utilice variables que permitan ingresar los datos de ambas matrices y se utilizo for  y también se utilizo un cin para ir guardando los datos ingresados se almacena en los punteros.

--- colaborador1.h
+++ colaborador1.h
@@ -2,6 +2,21 @@ void ingresar(float (*m1)[5], float (*m2)[5])
 {
 
 cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
+cout<<"\nDigitando los elementos de la primera matriz: \n";
+for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"Digite un numero["<<i<<"]["<<j<<"]: ";
+			cin>>*(*(m1+i)+j); 
+		}
+	}
+cout<<"\nIngrese valores de la matriz2:\n";
+ for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"Digite un numero["<<i<<"]["<<j<<"]: ";
+			cin>>*(*(m2+i)+j); 
+		}
+	}
+
 
 
 
