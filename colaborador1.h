a23d526a138f8c09f878d0bab80249cb74761910
Montaño Solis Luis <86112119+luisolis20@users.noreply.github.com>
2021-07-09 09:30:06 -0500

    Add files via upload
    Cada Colaborador editará su parte

---
+++ colaborador1.h
@@ -0,0 +1,41 @@
+void ingresar(float (*m1)[5], float (*m2)[5])
+{
+
+cout<<"Ingresar y mostrar los valores de los arreglos o matrices";
+cout<<"\nDigitando los elementos de la primera matriz: \n";
+for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"Digite un numero["<<i<<"]["<<j<<"]: ";
+			cin>>*(*(m1+i)+j); 
+		}
+	}
+cout<<"\nIngrese valores de la matriz2:\n";
+ for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout<<"Digite un numero["<<i<<"]["<<j<<"]: ";
+			cin>>*(*(m2+i)+j); 
+		}
+	}
+
+
+
+
+
+}
+void mostrar(float (*m1)[5],float (*m2)[5]){
+	cout<<"\nElementos de la matriz 1 : \n";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m1[i][j]<<" | ";
+	}
+cout<<"\n";
+	}
+	cout<<"\nElementos de la matriz 2: \n";
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      cout<<m2[i][j]<<" | ";
+	}
+cout<<"\n";
+	}
+
+}
