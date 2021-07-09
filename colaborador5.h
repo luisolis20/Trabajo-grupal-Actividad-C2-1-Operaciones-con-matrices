dfc3a7ab18b431774553110d7485f8554c6c825c
Montaño Solis Luis <86112119+luisolis20@users.noreply.github.com>
2021-07-09 09:55:22 -0500

    Update colaborador5.h
    Para poder calcular la media y varianza de las dos matrices declaré las siguientes variables de tipo float  (a,a1,v1,c,me1,a2,a3,c1,v2,me2) estas variables me permitieron poder calcular con mejor precisión la media y la varianza de ambas matrices. Luego para poder calcular la media se utilicé dos for, el primer for va a ir aumentando las filas y el segundo for va a ir aumentando las columnas, Luego hice el llamado de una variable (a) la cual va a ir sumando todos los elementos de la primera matriz y agregué un contador que va a ir aumentando de uno en uno, y luego hice el llamado de una variable (me1) la cual va a hacer el proceso de calcular la media de la primera matriz ; el mismo proceso se realicé para calcular la media de la segunda matriz pero haciendo el llamado de las otras variables
    Luego para calcular la varianza utilicé 2 ciclos for anidados que harán el proceso de ir aumentando las filas y columnas, y dentro de ese ciclo hice el proceso de multiplicar la matriz 1 dos veces y luego que me vaya restando la media de la primera matriz y que después me la sume.
    Y agregué una condición que me va a evaluar cual de las dos matrices tiene mayor media y varianza

--- colaborador5.h
+++ colaborador5.h
@@ -1,8 +1,65 @@
 void mediayvarianza(float (*m1)[5], float (*m2)[])
 {
+  float a,a1,v1,c,me1,a2,a3,c1,v2,me2;
+	a=0;
+	a1=0;
+	c=0;
+	a2=0;
+	a3=0;
+	c1=0;
 
 cout<<"Determinar  cual de las 2 matrices tiene la mayor media y varianza";
-
+for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	     
+	     a += *(*(m1+j)+i);
+	     c += 1;
+	}
+	}
+	me1=a/c;
+	cout<<"La media de la primera matriz es: "<<me1<<endl;
+	
+	for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	     
+	     a1 += (*(*(m1+j)+i)-me1)*(*(*(m1+j)+i)-me1);
+	}
+	}
+	v1=a1/c;
+	cout<<"La varianza de la primera matriz es: "<<v1<<endl;
+	
+	for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	      
+	     a2 += *(*(m2+j)+i);
+	     c1 += 1;
+	}
+	}
+	me2=a2/c1;
+	cout<<"La media de la segunda matriz es: "<<me2<<endl;
+	
+	for(int i=0;i<5;i++){
+	for(int j=0;j<5;j++){
+	     
+	     a3 += (*(*(m2+j)+i)-me2)*(*(*(m2+j)+i)-me2);
+	}
+	}
+	v2=a3/c1;
+	cout<<"La varianza de la segunda matriz es: "<<v2<<endl;
+	if (me1>me2){
+	cout<<" La Primera matriz tiene mayor media   "<<endl;
+	}
+	else{
+		cout<<" La Segunda matriz tiene mayor media   "<<endl;
+		
+	}
+if (v1>v2){
+	cout<<" La Primera matriz tiene mayor varianza  "<<endl;
+	}
+	else{
+		cout<<" La Segunda matriz tiene mayor varianza  "<<endl;
+		
+	}
 
 
 
